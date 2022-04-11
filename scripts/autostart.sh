573a32b1939f5e7504aead48b83219368d9f252a
Şahin Akkaya <sahin@sahinakkaya.dev>
2022-04-12 02:13:05 +0300

    Remove vsync while autostarting

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -11,9 +11,11 @@ function run {
 
 rm ~/.cache/workspaces
 nitrogen --save --set-zoom-fill ~/Pictures/Wallpapers/default.jpg
-run picom --experimental-backends --vsync -b # should prevent screen tearing on most setups if needed
+run picom --experimental-backends -b # --vsync should prevent screen tearing on most setups if needed
 
 eww -c ~/.config/eww/bar/ close-all
 eww -c ~/.config/eww/bar/ open bar
+
+keynav daemonize
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from GNOME
